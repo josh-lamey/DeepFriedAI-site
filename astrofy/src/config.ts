36e05137c183f0a7e1fcc8b3ec2ad6825db3a8ce
Josh Lamey <josh.lamey@deepfriedai.com>
2024-05-22 14:44:46 -0400

    initial commit

---
+++ astrofy/src/config.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'DeepFriedAI.com | Professional and Personal Site of Josh Lamey';
+export const SITE_DESCRIPTION = 'Navigate the AI landscape with DeepFriedAI.com. We serve up fresh, crispy insights on artificial intelligence, machine learning, and data science. Stay updated with content tailored to both beginners and experts, making AI accessible to all.';
+export const GENERATE_SLUG_FROM_TITLE = true
+export const TRANSITION_API = true
